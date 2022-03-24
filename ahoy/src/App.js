8899ec164882c481dc1479f57891c4b13c964155
Lycores <rcmao97@gmail.com>
2022-03-24 22:06:48 +0800

    make sure left distance is kept (300px)

--- ahoy/src/App.js
+++ ahoy/src/App.js
@@ -1,27 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  
   return (
     <div className="App">
       <div className="App-header">
        <div className="container">
-        <div className="cardContainer">
-          <div className="card-1"></div>
-          <div className="card-2"></div>
-          <div className="card-3"></div>
+        <div className="cardContainer" id="cardLeftDistance" >
+          <div className="card-1 ">Down Coat</div>
+          <div className="card-2">Warm</div>
+          <div className="card-3">Cold</div>
         </div>
 
-        <div className="cardContainer">
-          <div className="card-1"></div>
-          <div className="card-2"></div>
-          <div className="card-3"></div>
+        <div className="cardContainer" onClick={onclick}>
+          <div className="card-1">Sweater</div>
+          <div className="card-2">Warm</div>
+          <div className="card-3">Cold</div>
         </div>
           
         <div className="cardContainer">
-          <div className="card-1"></div>
-          <div className="card-2"></div>
-          <div className="card-3"></div>
+          <div className="card-1">Snow Boot</div>
+          <div className="card-2">Necessary</div>
+          <div className="card-3">Unnecessary</div>
+        </div>
+
+        <div className="cardContainer">
+          <div className="card-1">Rain Boot</div>
+          <div className="card-2">Necessary</div>
+          <div className="card-3">Unnecessary</div>
+        </div>
+
+        <div className="cardContainer">
+          <div className="card-1">Mask</div>
+          <div className="card-2">Necessary</div>
+          <div className="card-3">Unnecessary</div>
         </div>
        </div>
       </div>

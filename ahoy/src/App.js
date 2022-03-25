77ce415483fb54193245e0a251b8123c0c1aa4be
Lycores <rcmao97@gmail.com>
2022-03-25 15:30:40 +0800

    现在所有stack可以全部展开

--- ahoy/src/App.js
+++ ahoy/src/App.js
@@ -18,51 +18,79 @@ function App() {
     return (event) => {
       switch(componentClicked){
         case "umbrella":
-          console.log(allCardsContainers)
+          setAllCardsContainers(
+            {...allCardsContainers, umbrellaStyle: {...allCardsContainers.umbrellaStyle, marginRight:"300px"}}
+          )
+          break
+        case "downCoat":
+          setAllCardsContainers(
+            {...allCardsContainers, downCoatStyle: {...allCardsContainers.downCoatStyle, marginRight:"300px"}}
+          )
+          break
+        case "sweater":
+          setAllCardsContainers(
+            {...allCardsContainers, sweaterStyle: {...allCardsContainers.sweaterStyle, marginRight:"300px"}}
+          )
+          break
+        case "snowBoot":
+          setAllCardsContainers(
+            {...allCardsContainers, snowBootStyle: {...allCardsContainers.snowBootStyle, marginRight:"300px"}}
+          )
+          break
+        case "rainBoot":
+          setAllCardsContainers(
+            {...allCardsContainers, rainBootStyle: {...allCardsContainers.rainBootStyle, marginRight:"300px"}}
+          )
+          break
+        case "mask":
+          setAllCardsContainers(
+            {...allCardsContainers, maskStyle: {...allCardsContainers.maskStyle, marginRight:"300px"}}
+          )
+        break
       }
     }
   }
   
-  const [allCardsContainers, setAllCardsContainers] = useState({
+  let [allCardsContainers, setAllCardsContainers] = useState({
     umbrellaStyle, downCoatStyle, sweaterStyle, snowBootStyle, rainBootStyle, maskStyle
   })
-  
+
   return (
     <div className="App">
       <div className="App-header">
        <div className="container cardLeftDistance">
-        <div ref={umbrellaRef}   style={umbrellaStyle}  onClick={clickCardContainer("umbrella")}>
+        <div ref={umbrellaRef}   style={allCardsContainers.umbrellaStyle}  onClick={clickCardContainer("umbrella")}>
           <Card name="Umbrella" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={downCoatRef} style={downCoatStyle} onClick={clickCardContainer("downCoat")} >
+        <div ref={downCoatRef} style={allCardsContainers.downCoatStyle} onClick={clickCardContainer("downCoat")} >
           <Card name="Down Coat" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={sweaterRef} style={sweaterStyle}  onClick={clickCardContainer("sweater")}>
+        <div ref={sweaterRef} style={allCardsContainers.sweaterStyle}  onClick={clickCardContainer("sweater")}>
           <Card name="Sweater" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
           
-        <div ref={snowBootRef} style={snowBootStyle} onClick={clickCardContainer("snowBoot")}>
+        <div ref={snowBootRef} style={allCardsContainers.snowBootStyle} onClick={clickCardContainer("snowBoot")}>
           <Card name="Snow Boot" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
           
         </div>
 
-        <div ref={rainBootRef} style={rainBootStyle} onClick={clickCardContainer("rainBoot")}>
+        <div ref={rainBootRef} style={allCardsContainers.rainBootStyle} onClick={clickCardContainer("rainBoot")}>
           <Card name="Rain Boot" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={maskRef} style={maskStyle} onClick={clickCardContainer("mask")}>
+        <div ref={maskRef} style={allCardsContainers.maskStyle} onClick={clickCardContainer("mask")}>
           <Card name="Mask" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>

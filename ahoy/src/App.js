b82f0a26519d08a2fde0a733fb7d9cbd3c981b09
Lycores <rcmao97@gmail.com>
2022-03-25 11:58:15 +0800

    make style of cardcontainer state based

--- ahoy/src/App.js
+++ ahoy/src/App.js
@@ -14,48 +14,81 @@ function App() {
   const maskRef = useRef(null)
 
   const expandCard = () => {
-    if(umbreallaRef.current.className.indexOf("expandCard") <= -1){
-      umbreallaRef.current.className = umbreallaRef.current.className + " expandCard"
-    }
-    console.log(umbreallaRef.current)
+    console.log(useRef)
   }
 
+  const cardContainer = {
+    width: '200px',
+    height: '200px',
+    margin: '50px',
+    position:'relative'
+  }
+
+  let umbrellaStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
+
+  let downCoatStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
+
+  let sweaterStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
+
+  let snowBootStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
+
+  let rainBootStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
+
+  let maskStyle = {
+    ...cardContainer,
+    marginRight: '50px'
+  }
   return (
     <div className="App">
       <div className="App-header">
-       <div className="container">
-        <div ref={umbreallaRef} className="cardContainer" id="cardLeftDistance" onClick={expandCard}>
+       <div className="container cardLeftDistance">
+        <div ref={umbreallaRef}   style={umbrellaStyle}  onClick={expandCard}>
           <Card name="Umbrella" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={downCoatRef} className="cardContainer"  >
+        <div ref={downCoatRef} style={downCoatStyle}  >
           <Card name="Down Coat" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={sweaterRef} className="cardContainer"  onClick={onclick}>
+        <div ref={sweaterRef} style={sweaterStyle}  onClick={onclick}>
           <Card name="Sweater" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
           
-        <div ref={snowBootRef} className="cardContainer">
+        <div ref={snowBootRef} style={snowBootStyle}>
           <Card name="Snow Boot" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
           
         </div>
 
-        <div ref={rainBootRef} className="cardContainer">
+        <div ref={rainBootRef} style={rainBootStyle}>
           <Card name="Rain Boot" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>
         </div>
 
-        <div ref={maskRef} className="cardContainer">
+        <div ref={maskRef} style={maskStyle}>
           <Card name="Mask" css="card-1"/>
           <Card name="Good" css="card-2"/>
           <Card name="Bad" css="card-3"/>

d74f832b7dc735394a726db459491bc209795529
Lycores <rcmao97@gmail.com>
2022-03-26 19:49:48 +0800

    inject props into cards

--- ahoy/src/App.js
+++ ahoy/src/App.js
@@ -55,45 +55,81 @@ function App() {
     umbrellaStyle, downCoatStyle, sweaterStyle, snowBootStyle, rainBootStyle, maskStyle
   })
 
+  let [umbrellaCardProps, setUmbrellaCardProps] = useState({
+    card_1_props: {name: "Umbrella", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
+  let [downCoatProps, setDownCoatProps] = useState({
+    card_1_props: {name: "DownCoat", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
+  let [sweaterProps, setSweaterProps] = useState({
+    card_1_props: {name: "Sweater", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
+  let [snowBootProps, setSnowBootProps] = useState({
+    card_1_props: {name: "SnowBoot", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
+  let [rainBootProps, setRainBootProps] = useState({
+    card_1_props: {name: "RainBoot", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
+  let [maskProps, setMaskProps] = useState({
+    card_1_props: {name: "Mask", css: "card-1"},
+    card_2_props: {name: "Good", css: "card-2"},
+    card_3_props: {name: "Good", css: "card-3"},
+  })
+
   return (
     <div className="App">
       <div className="App-header">
        <div className="container cardLeftDistance">
         <div ref={umbrellaRef}   style={allCardsContainers.umbrellaStyle}  onClick={clickCardContainer("umbrella")}>
-          <Card name="Umbrella" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
+          <Card {...umbrellaCardProps.card_1_props}/>
+          <Card {...umbrellaCardProps.card_2_props}/>
+          <Card {...umbrellaCardProps.card_3_props}/>
         </div>
 
         <div ref={downCoatRef} style={allCardsContainers.downCoatStyle} onClick={clickCardContainer("downCoat")} >
-          <Card name="Down Coat" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
+          <Card {...downCoatProps.card_1_props}/>
+          <Card {...downCoatProps.card_2_props}/>
+          <Card {...downCoatProps.card_3_props}/>
         </div>
 
         <div ref={sweaterRef} style={allCardsContainers.sweaterStyle}  onClick={clickCardContainer("sweater")}>
-          <Card name="Sweater" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
+          <Card {...sweaterProps.card_1_props}/>
+          <Card {...sweaterProps.card_2_props}/>
+          <Card {...sweaterProps.card_3_props}/>
         </div>
           
         <div ref={snowBootRef} style={allCardsContainers.snowBootStyle} onClick={clickCardContainer("snowBoot")}>
-          <Card name="Snow Boot" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
-          
+          <Card {...snowBootProps.card_1_props}/>
+          <Card {...snowBootProps.card_2_props}/>
+          <Card {...snowBootProps.card_3_props}/>
         </div>
 
+
         <div ref={rainBootRef} style={allCardsContainers.rainBootStyle} onClick={clickCardContainer("rainBoot")}>
-          <Card name="Rain Boot" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
+          <Card {...rainBootProps.card_1_props}/>
+          <Card {...rainBootProps.card_2_props}/>
+          <Card {...rainBootProps.card_3_props}/>
         </div>
 
         <div ref={maskRef} style={allCardsContainers.maskStyle} onClick={clickCardContainer("mask")}>
-          <Card name="Mask" css="card-1"/>
-          <Card name="Good" css="card-2"/>
-          <Card name="Bad" css="card-3"/>
+          <Card {...maskProps.card_1_props}/>
+          <Card {...maskProps.card_2_props}/>
+          <Card {...maskProps.card_3_props}/>
         </div>
        </div>
       </div>

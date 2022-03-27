0523f5a793acdcefe670a820ab7d233c12963909
Lycores <rcmao97@gmail.com>
2022-03-27 23:31:48 +0800

    try catch requestDesktopData

--- ahoy_frontend/src/allRequest.js
+++ ahoy_frontend/src/allRequest.js
@@ -1,10 +1,15 @@
 const requestDesktopData = async (url) => {
+    try{
         const res = await fetch(url, {method: 'GET'})
         .then(response => response.json())
         .then(json => {
             return json;
         })
-        console.log(res)
+        console.log(res)}
+    catch(e){
+        console.log(e)
+    }
+        
 }
 
 export default requestDesktopData

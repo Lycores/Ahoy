a34012e4e17dcecd85ea78fed6be4bd99bd99cfa
Lycores <rcmao97@gmail.com>
2022-03-27 22:41:32 +0800

    proxy successful but there are a lot of confusion

--- ahoy_frontend/src/allRequest.js
+++ ahoy_frontend/src/allRequest.js
@@ -1,8 +1,9 @@
 const requestDesktopData = (url) => {
     console.log(url)
-    fetch("http://localhost:4000", {method: 'GET'})
+    fetch("/api", {method: 'GET'})
     .then(response => response.text())
   .then(json => console.log(json))
+  
 }
 
 export default requestDesktopData

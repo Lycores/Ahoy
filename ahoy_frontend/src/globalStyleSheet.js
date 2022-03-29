8626bd061729618c7c88a0523b3f918430d0fdc1
Lycores <rcmao97@gmail.com>
2022-03-30 02:11:54 +0800

    Skeuomorphism finished

--- ahoy_frontend/src/globalStyleSheet.js
+++ ahoy_frontend/src/globalStyleSheet.js
@@ -7,7 +7,7 @@ export const globalStyle = {
     headerItemHeight: 70,
     homeButtonWidth:70,
     loginButtonWidth:70,
-    boxShadow: "18px 18px 30px rgba(0, 0, 0, 0.2)",
+    boxShadow: '18px 18px 30px rgba(0, 0, 0, 0.2), -18px -18px 30px rgba(255, 255, 255, 1)',
     borderRadius: 20,
     playlistWidth: 200
 }

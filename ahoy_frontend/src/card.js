520b8d363174cc0136d535418909311d67163709
Lycores <rcmao97@gmail.com>
2022-03-30 17:19:21 +0800

    tiny push

--- ahoy_frontend/src/card.js
+++ ahoy_frontend/src/card.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Card = React.forwardRef((props, ref) => {
     const {name, css, click} = props
-    console.log(name)
     return(
         <div ref = {ref} className={css} id={name} onClick={click}></div>
     )

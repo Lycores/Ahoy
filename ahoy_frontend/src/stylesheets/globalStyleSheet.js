bba1c5d381113c946a36f14b2f772d214459c63d
Lycores <rcmao97@gmail.com>
2022-04-02 00:59:43 +0800

    add spotify color

--- ahoy_frontend/src/stylesheets/globalStyleSheet.js
+++ ahoy_frontend/src/stylesheets/globalStyleSheet.js
@@ -10,7 +10,8 @@ export const globalStyle = {
     boxShadow: '18px 18px 30px rgba(0, 0, 0, 0.2), -18px -18px 30px rgba(255, 255, 255, 1)',
     borderRadius: 20,
     playlistAreaWidth:250,
-    globalBackgroundColor: "#efeeee"
+    globalBackgroundColor: "#efeeee",
+    spotifyColor:"1bd760"
 }
 
 export const borderRadius = "20px"

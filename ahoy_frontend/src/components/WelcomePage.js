145a6c7a0e454ef73557f44da792d1c0a305dece
Lycores <rcmao97@gmail.com>
2022-04-02 00:50:47 +0800

    welcomePage finished

--- ahoy_frontend/src/components/WelcomePage.js
+++ ahoy_frontend/src/components/WelcomePage.js
@@ -4,21 +4,29 @@ import {useNavigate} from 'react-router-dom'
 function WelcomePage() {
 
     const backgroundStyle= {
-        backgroundColor:  globalStyle.globalBackgroundColor
+        backgroundColor:  globalStyle.globalBackgroundColor,
+        width: '100vw',
+        height: '100vh'
     }
 
     const welcomeImage = {
+        marginLeft: "auto",
+        marginRight: "auto",
         width: 300,
         height: 300,
-        backgroundImage: `url(${spotify})`
+        backgroundImage: `url(${spotify})`,
+        backgroundSize: '80%'
     }
 
     const navigate = useNavigate()
 
     return(
         <div style={backgroundStyle}>
-            <div style={welcomeImage} onClick={() => {navigate('/index')}}>
-            </div>
+            <a href="/auth/login">
+                <div style={welcomeImage}/>
+            </a>
+            
+          
         </div>
     )
 }

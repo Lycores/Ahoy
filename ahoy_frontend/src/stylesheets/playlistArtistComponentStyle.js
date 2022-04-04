3c9d4ac18a4e6602e838fb0dd6650759b99e00a9
Lycores <rcmao97@gmail.com>
2022-04-04 22:00:06 +0800

    ranme

---
+++ ahoy_frontend/src/stylesheets/playlistArtistComponentStyle.js
@@ -0,0 +1,7 @@
+const playlistArtistComponentStyle = {
+    height: 30,
+    width: 100,
+    backgroundColor: 'black'
+}
+
+export default playlistArtistComponentStyle
--- ahoy_frontend/src/stylesheets/userSavedAlbumObjectStyleSheet.js
+++
@@ -1,7 +0,0 @@
-const userSavedAlbumObjectStyle = {
-    height: 30,
-    width: 100,
-    backgroundColor: 'black'
-}
-
-export default userSavedAlbumObjectStyle

170ec422e181d491c92a38c070efaa035e266494
Lycores <rcmao97@gmail.com>
2022-04-16 19:03:12 +0800

    we have title right now

--- ahoy_frontend/src/components/RightAreaComponentsForTracks.js
+++ ahoy_frontend/src/components/RightAreaComponentsForTracks.js
@@ -2,8 +2,9 @@ import 'bootstrap/dist/css/bootstrap-grid.css';
 import TrackEntryComponent from './TrackEntryComponent'
 import {albumOverviewStyle, coverStyle, albumDescriptionStyle, backgroundFilterStyle} from '../stylesheets/mainBodyStyle/rightAreaStyle/albumOverviewStyle'
 import globalStyle from '../stylesheets/globalStyle/globalStyleSheet';
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import TrackListComponent from './TrackListComponent'
+import {DesktopOrTablet, Mobile} from '../MediaQuery'
 
 function RightAreaComponentForTracks(props) {
     
@@ -11,10 +12,15 @@ function RightAreaComponentForTracks(props) {
     // var {tracks, trackCover, setTrack} = props
     
     let heightOfTracks = `calc(100% - ${albumOverviewStyle.height}px) `
-    
 
     var [coverStyleState, setCoverStyleState] = useState(coverStyle)
-    var [albumOverviewStyleStyleState, setAlbumOverviewStyleStyleState] = useState(albumOverviewStyle)
+    var [albumOverviewStyleState, setAlbumOverviewStyleStyleState] = useState(albumOverviewStyle)
+
+    const albumOverviewRef =  useRef(null)
+    // const widthOfTitleAndSubtitle = albumOverviewRef.current.offsetWidth
+    // const changeWidthOfTitleAndSubtitle= ()=> {
+    //     widthOfTitleAndSubtitle  = albumOverviewRef.current.offsetWidth
+    // }
 
     useEffect(()=>{
         setCoverStyleState({
@@ -22,18 +28,25 @@ function RightAreaComponentForTracks(props) {
             backgroundImage: `url(${album.images[1].url})`
         })
         setAlbumOverviewStyleStyleState({
-            ...albumOverviewStyleStyleState,
+            ...albumOverviewStyleState,
             backgroundImage: `url(${album.images[0].url})`,
             backgroundSize: 'cover'
         })
+        // window.addEventListener('resize', changeWidthOfTitleAndSubtitle);
+        console.log(albumOverviewRef.current.offsetWidth)
     },[album])
         
         
     return(
         <div style={{height: '100%', overflow: 'scroll'}}>
-            <div style={albumOverviewStyleStyleState}>
+            <div ref={albumOverviewRef} style={albumOverviewStyleState}>
                 <div style={backgroundFilterStyle}>
-                    <div style={albumDescriptionStyle}></div>
+                    <DesktopOrTablet>
+                        <div style={albumDescriptionStyle}>
+                            <div style={{fontSize: '400%', width:'100%', marginTop:'100px', textAlign:'right', whiteSpace:'nowrap'}}>{album.name}</div>
+                        </div>
+                    </DesktopOrTablet>
+                    
                     <div style={coverStyleState}></div>
                     
                 </div>
